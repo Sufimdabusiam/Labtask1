b49cc2142ac616856e5d9b957382a48dea4cd6f1
sufimdabusiam <sufimdabusiam0@gmail.com>
2020-02-10 12:16:06 +0600

    This is my first labtask

---
+++ src/IfElse.java
@@ -0,0 +1,12 @@
+public class IfElse{
+
+	public static void main(String[]args) {
+		
+		   int age=30;   
+		    if(age>18){  
+		        System.out.print("Age is greater than 18");  
+		    }  
+		    else 
+		    	System.out.println("Age is greater than 30");
+  }
+}
